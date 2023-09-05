b14f063421a1999f45db30bcddd08cc8a2c4b188
yerly73 <yerly.aysa@tecsup.edu.pe>
2023-09-05 12:58:03 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.sendFile('./static/formulario.html', {
+        root: __dirname
+    });
+});
+
+
+app.get('/contact', (req, res) => {
+    res.sendFile('./static/contact.html', {
+        root: __dirname
+    });
+});
+
+// Manejo de error 404
+app.use((req, res) => {
+    res.status(404).send('No se encontró la página...');
+});
+
+app.listen(3000);
+console.log(`Server on port ${3000}`);
